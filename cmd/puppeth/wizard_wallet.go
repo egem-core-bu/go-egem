7048ecc17ae16f80535984ae53022695855a6831
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-10-10 08:49:32 -0700

    default port change for next release.

--- cmd/puppeth/wizard_wallet.go
+++ cmd/puppeth/wizard_wallet.go
@@ -46,7 +46,7 @@ func (w *wizard) deployWallet() {
 	infos, err := checkWallet(client, w.network)
 	if err != nil {
 		infos = &walletInfos{
-			nodePort: 30303, rpcPort: 8545, webPort: 80, webHost: client.server,
+			nodePort: 30666, rpcPort: 8895, webPort: 80, webHost: client.server,
 		}
 	}
 	existed := err == nil
