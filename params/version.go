10ed0c61467dc57855d4b98fd3aa982fe3b6d87c
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-11-26 07:59:53 -0800

    version bump

--- params/version.go
+++ params/version.go
@@ -22,9 +22,9 @@ import (
 
 const (
 	VersionMajor = 1          // Major version component of the current release
-	VersionMinor = 0          // Minor version component of the current release
-	VersionPatch = 5          // Patch version component of the current release
-	VersionMeta  = "nikola"   // Version metadata to append to the version string
+	VersionMinor = 1          // Minor version component of the current release
+	VersionPatch = 0          // Patch version component of the current release
+	VersionMeta  = "tolkien"   // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
