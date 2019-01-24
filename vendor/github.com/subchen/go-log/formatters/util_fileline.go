4eb73c4ac60d1f9c4a504dfa6ee5242a81f62744
Derek May <32908855+riddlez666@users.noreply.github.com>
2019-01-24 11:35:28 -0800

    go-log fix

---
+++ vendor/github.com/subchen/go-log/formatters/util_fileline.go
@@ -0,0 +1,34 @@
+package formatters
+
+import (
+	"runtime"
+	"strings"
+)
+
+// FilelineCaller returns file and line for caller
+func FilelineCaller(skip int) (file string, line int) {
+	for i := 0; i < 10; i++ {
+		_, file, line, ok := runtime.Caller(skip + i)
+		if !ok {
+			return "???", 0
+		}
+
+		// file = pkg/file.go
+		n := 0
+		for i := len(file) - 1; i > 0; i-- {
+			if file[i] == '/' {
+				n++
+				if n >= 2 {
+					file = file[i+1:]
+					break
+				}
+			}
+		}
+
+		if !strings.HasPrefix(file, "go-log/") {
+			return file, line
+		}
+	}
+
+	return "???", 0
+}
